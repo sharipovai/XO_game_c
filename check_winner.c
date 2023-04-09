c3c4629eb761d2bce97449ee7084460e626b6230
User <artursharipv@mail.ru>
2023-04-09 21:33:03 +0400

    change chulpan alghoritm

--- check_winner.c
+++ check_winner.c
@@ -1,40 +1,41 @@
+#include <math.h>
 #include <stdio.h>
+#define N 3
 
-int check_winner(int arr[3][3]);
+int check_winner(int arr[N][N]);
 
 int main() {
-    int arr[3][3] = {0};
+    int arr[N][N] = {0};
     arr[0][2] = 2; arr[1][1] = 2; arr[2][0] = 2;
     printf("%d", check_winner(arr));
 }
 
-int check_winner(int arr[3][3]) {
+int check_winner(int arr[N][N]) {
     int res = 0;
-    // Проверяем строки
-    for (int i = 0; i < 3; i++) {
-        if (arr[i][0] == 1 && arr[i][1] == 1 && arr[i][2] == 1) {
-            res = 1;
-        }
-        if (arr[i][0] == 2 && arr[i][1] == 2 && arr[i][2] == 2) {
-            res = 2;
-        }
+    int n = N;
+    int mult_diag_rl = 1;
+    int mult_diag_lr = 1;
+    int pow_value = 2 * 2 * 2;
+    for (int i = 0; i < n; i++) {
+        int mult_vert = 1;
+	int mult_gor = 1;
+        for (int j = 0; j < n; j++) {
+	    mult_vert *= arr[j][i];
+	    mult_gor *= arr[i][j];
+	}
+	if (mult_vert == 1 || mult_gor == 1) {
+	    res = 1;
+	}
+	if (mult_vert == pow_value || mult_gor == pow_value) {
+	    res = 2;
+	}
+	mult_diag_lr *= arr[i][i];
+        mult_diag_rl *= arr[i][n - i - 1];
     }
-    // Проверяем столбцы
-    for (int j = 0; j < 3; j++) {
-        if (arr[0][j] == 1 && arr[1][j] == 1 && arr[2][j] == 1) {
-            res = 1;
-        }
-        if (arr[0][j] == 2 && arr[1][j] == 2 && arr[2][j] == 2) {
-            res = 2;
-        }
-    }
-    // Проверяем диагонали
-    if ((arr[0][0] == 1 && arr[1][1] == 1 && arr[2][2] == 1) || (arr[0][2] == 1 && arr[1][1] == 1 && arr[2][0] == 1)) {
+    if (mult_diag_lr == 1 || mult_diag_rl == 1) {
         res = 1;
-    }
-    if ((arr[0][0] == 2 && arr[1][1] == 2 && arr[2][2] == 2) || (arr[0][2] == 2 && arr[1][1] == 2 && arr[2][0] == 2)) {
+    } else if (mult_diag_lr == pow_value || mult_diag_rl == pow_value) {
         res = 2;
     }
-
     return res;
 }
