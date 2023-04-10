3747729798a6eb46980f3f7f211e7514a03577d1
Hema Malini <my@my.com>
2023-04-10 16:24:21 +0300

    moya func

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// void congratulations(int x);
+
+// int main () {
+//     scanf("%d", &x);
+//     congratulations(int x);
+
+//     return 0;
+// }
+
+
+void congratulations(int x){
+    if (x == 1) {
+        printf("Конец игры! Победил игрок 1");
+    } else if(x == 2) {
+        printf("Конец игры! Победил игрок 2");
+    } else {
+        printf("Победителей нет")
+    }
+    
+}
