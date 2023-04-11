6eb3a5f22f916295f1e06fc64ba446b9b93ca479
Alex000S <sasha00a@yandex.ru>
2023-04-11 17:34:19 -0400

    Eche chut-chut

--- main.c
+++ main.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
-void view_board(int array[3][3]);
+void view_board(int array[10][10]);
 int main(void) {
-	int array[3][3];
+	int array[10][10];
 	view_board(array);
 	return 0;
 }
-	void view_board(int array[][3]){
+	void view_board(int array[][10]){
 	array[1][1] = 1;
-	array[1][2] = 1;
+	array[1][2] = 3;
 	array[1][3] = 1;
+	array[1][4] = 3;
+	array[1][5] = 1;
 	array[2][1] = 2;
-	array[2][2] = 2;
+	array[2][2] = 3;
 	array[2][3] = 2;
+	array[2][4] = 3;
+	array[2][5] = 2;
 	array[3][1] = 1;
-	array[3][2] = 1;
+	array[3][2] = 3;
 	array[3][3] = 1;
+	array[3][4] = 3;
+	array[3][5] = 1;
 	int i;
     	int j;
-    	for (j = 0; j < 3; j++) {
-        	for (i = 0; i < 3; i++) {
+    	for (j = 1; j < 4; j++) {
+        	for (i = 1; i < 6; i++) {
             	if (array[j][i] == 1)
                		printf("X");
-            	else
+		if (array[j][i] == 3)
+			printf("|");
+	
+		if (array[j][i] == 2)
                 	printf("0");
         	}
         	printf("\n");
+	
    	 }
+
 }
